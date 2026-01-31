90e6a3f1b5a954ab2875a89dbdec8c3422c21fe3
kartikay singh <kartikaysin566@gmail.com>
2026-01-31 10:22:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    float f1= 1.5f;
+	    float f2= 2.0f;
+	    float p=  f1 *  f2;
+		System.out.println("The product is :" +p);
+	}
+}
